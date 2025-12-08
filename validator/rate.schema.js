a0aac616d74637d792fea2e1519001be4a59eeaf
Cristian <cristian.vcabezas@hotmail.com>
2025-12-08 13:22:36 -0500

    let 0 values on rates

--- validator/rate.schema.js
+++ validator/rate.schema.js
@@ -3,7 +3,7 @@ import z from "zod";
 export const rateRegisterSchema = z.object({
     v_name: z.string().min(1, "El nombre es obligatorio"),
     v_pax: z.number().int().positive("El número de personas debe ser positivo"),
-    v_value4: z.number().positive("El valor por 4 horas debe ser positivo"),
+    v_value4: z.number().nonnegative("El valor por 4 horas debe ser positivo"),
     v_value8: z.number().positive("El valor por 8 horas debe ser positivo"),
     v_value_extra: z.number().positive("El valor extra debe ser positivo"),
     v_isPartner: z.boolean(),
@@ -15,7 +15,7 @@ export const rateUpdateSchema = z.object({
     v_id_rate: z.number().int().positive("El ID de la tarifa es obligatorio"),
     v_name: z.string().min(1, "El nombre es obligatorio"),
     v_pax: z.number().int().positive("El número de personas debe ser positivo"),
-    v_value4: z.number().positive("El valor por 4 horas debe ser positivo"),
+    v_value4: z.number().nonnegative("El valor por 4 horas debe ser positivo"),
     v_value8: z.number().positive("El valor por 8 horas debe ser positivo"),
     v_value_extra: z.number().positive("El valor extra debe ser positivo")
 }).strict();
